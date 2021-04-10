a531d58bc7e8db64600afa89730ef8495c80f8db
Michael Martinez <zeroeclipse@live.com>
2021-04-09 17:02:06 -0700

    Added default profile pictures.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,12 +1,16 @@
 class FriendsController < ApplicationController
+
     def index
         @friends = current_user.friends
     end
+
     def create
         request = Request.find_by_id(params[:id])
         request.update(confirmed: true)
         redirect_to requests_path, success: "Friend added!"
     end
+
     def destroy
     end
+    
 end
